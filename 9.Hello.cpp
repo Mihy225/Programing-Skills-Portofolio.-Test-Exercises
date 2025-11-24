e64b902e6ad9d596597f8ed581cdc7e9efcd6ed8
Mihy225 <mihaelamazilu97@gmail.com>
2025-11-24 04:06:03 +0000

    Add files via upload

---
+++ 9.Hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Function definition
+void hello() {
+    cout << "Hello" << endl;
+}
+
+int main() {
+    // Call the function
+    hello();
+
+    return 0;
+}
