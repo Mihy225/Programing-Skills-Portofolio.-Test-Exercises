d4d2c649d3565dc9b8cd7bb64e311491004970b0
Mihy225 <mihaelamazilu97@gmail.com>
2025-11-24 03:32:26 +0000

    Add files via upload

---
+++ 2.SimpleSums.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 8;   // first number
+    int y = 10;  // second number
+    int total = x + y;  // add them together
+    cout << "The sum is: " << total << endl;
+    return 0;
+}
